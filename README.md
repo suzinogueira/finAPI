b29db7dc498f0a1642070fb86a304de8abd4820b
Suzi Nogueira <asuzinogueira@gmail.com>
2021-11-30 23:55:17 -0300

    creating the first validations

---
+++ README.md
@@ -0,0 +1,18 @@
+## FinAPI - Financeira
+
+### Requisitos
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário ou cliente
+- [] 
+- [] 
+- [] 
+
+
+
+
+### Regras de negócio
+
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possível fazer depósito em uma conta não existente
+- [] Não deve ser possível  
+- [] Não deve ser possível  
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const { v4:uuidv4 } = require('uuid')
+
+const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const {cpf, name} = request.body;
+
+  const customerAlredyExists = customers.some((customer) => 
+    customer.cpf === cpf
+  );
+
+  if(customerAlredyExists){
+    return response.status(400).json({
+      error:"Customer Already Exists !"
+    })
+  }
+
+  customers.push({
+    cpf, 
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+
+})
+
+app.get("/statement", (request, response) => {
+  const {cpf} = request.headers;
+  const customer = customers.find(customer =>
+    customer.cpf === cpf)
+  
+  if(!customer){
+    return response.status(400).json({error: "Customer not found"})
+  }
+  return response.json(customer.statement)
+})
+
+app.listen(3330);
